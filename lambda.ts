82919637e590c2255e043c34aec24049922689df
chanell <ngchanell287@gmail.com>
2021-11-05 04:20:03 +0800

    update serverless

---
+++ lambda.ts
@@ -0,0 +1,27 @@
+/**import serverless from 'serverless-http';
+import { getConnection } from 'typeorm';
+import {app,db} from './index.js';
+import { setupDatabase } from './init/database';
+
+const appLambda = serverless(app);
+
+const ensureDatabaseConnection = async () => {
+  try {
+    getConnection();
+    console.log('Reusing old database connection');
+  } catch (err) {
+    console.log('Creating new database connection');
+    await setupDatabase();
+  }
+};
+
+export const handler: serverless.Handler = async (event, context) => {
+  // Make sure we have a database connection
+  await ensureDatabaseConnection();
+
+  // Handle the event
+  const result = await appLambda(event, context);
+
+  return result;
+};
+**/
