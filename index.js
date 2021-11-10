5f798d1cf3db16318cf8cd1e902383e5c23ae65e
chanell <ngchanell287@gmail.com>
2021-11-10 21:57:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const serverless = require('serverless-http');
 
 // Import routes
 let apiRoutes = require("./api-routes");
+// Initialize express router
+let router = require('express').Router();
+var quoteController = require('./quoteController');
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
     extended: true
@@ -23,7 +26,18 @@ var port = process.env.PORT || 8080;
 app.get('/', (req, res) => res.send('Do you want quotes?'));
 
 // Use Api routes in the App
-app.use('/api', apiRoutes);
+const apiRouter = router;
+app.use('/api', apiRouter);
+
+const quoteRouter = router;
+apiRouter.use('/quotes', quoteRouter);
+
+quoteRouter.get('/', quoteController.index)
+quoteRouter.post('/', quoteController.new);
+quoteRouter.get(':quote_id',quoteController.view)
+quoteRouter.patch(':quote_id',quoteController.update)
+quoteRouter.put(':quote_id',quoteController.update)
+quoteRouter.delete(':quote_id',quoteController.delete);
 // Launch app to listen to specified port
 if(!module.parent){
 app.listen(port, function () {
